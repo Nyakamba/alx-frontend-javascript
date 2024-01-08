02979491023e431cc5eccf44a82ba6aa37ce1f73
Nyakamba <omwegaenock@gmail.com>
2024-01-08 16:19:58 +0300

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,11 @@
-export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+export default function updateStudentGradeByCity(
+  listStudents,
+  city,
+  newGrades
+) {
   const students = listStudents.filter((student) => student.location === city);
   for (const student of students) {
-    student.grade = 'N/A';
+    student.grade = "N/A";
   }
   const newStudents = students.map((student) => {
     const single = student;
