b5a51e31793482657a84aa657f20f4830c3a87e2
Nyakamba <omwegaenock@gmail.com>
2024-01-10 15:43:38 +0300

    creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,36 +1,36 @@
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
 
 const student1: Student = {
-	firstName: "Zeliq",
-	lastName: "Shannon",
-	age: 40,
-	location: "Nakuru"
+  firstName: "Zeliq",
+  lastName: "Shannon",
+  age: 40,
+  location: "Nakuru",
 };
 
 const student2: Student = {
-        firstName: "John",
-        lastName: "Doe",
-        age: 37,
-        location: "Nairobi"
+  firstName: "John",
+  lastName: "Doe",
+  age: 37,
+  location: "Nairobi",
 };
 
 const studentsList: Student[] = [student1, student2];
 
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
 
 table.style.background = "pink";
 table.appendChild(tbody);
 
 studentsList.forEach((student: Student): void => {
-  const row = document.createElement('tr');
-  const nameCell = document.createElement('td');
-  const locationCell = document.createElement('td');
+  const row = document.createElement("tr");
+  const nameCell = document.createElement("td");
+  const locationCell = document.createElement("td");
 
   nameCell.textContent = student.firstName;
   locationCell.textContent = student.location;
--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
   entry: "./js/main.ts",
@@ -10,28 +10,28 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         options: {
-          transpileOnly: true
-        }
-      }
-    ]
+          transpileOnly: true,
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
   },
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Development"
-    })
+      title: "Development",
+    }),
   ],
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+  },
 };
